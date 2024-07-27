d8243fcaf249633b5c498a884854f8f6171b7799
Bismuth <68436600+BismuthBloom@users.noreply.github.com>
2024-07-27 17:16:36 -0400

    Added language selection

--- js/header.js
+++ js/header.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
         `
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
-                <div class="col-6" style="overflow: hidden;">
+                <div class="col-4" style="overflow: hidden;">
                     <a href="index.html"><img src="Assets/meLogo.svg" class="img-fluid" alt="MOTHER encore"></a>
                 </div>
-                <div class="col-6">
+                <div class="col-8 justify-content-end">
                     <button class="col navbar-toggler justify-content-right" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNavAltMarkup"
                             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"
@@ -31,6 +31,20 @@ $(document).ready(function () {
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link translated" data-id="SITE_NAV_CREDITS" href="credits.html">Credits</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Language
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="right:0; left: auto">
+                                        <a class="dropdown-item" id="en" onclick="setLanguage('en')">English</a>
+                                        <a class="dropdown-item" id="ja" onclick="setLanguage('ja')">日本語</a>
+                                        <a class="dropdown-item" id="fr" onclick="setLanguage('fr')">Français</a>
+                                        <a class="dropdown-item" id="es" onclick="setLanguage('es')">Español</a>
+                                        <a class="dropdown-item" id="pt_BR" onclick="setLanguage('pt_BR')">Português Brasil</a>
+                                        <a class="dropdown-item" id="it" onclick="setLanguage('it')">Italiano</a>
+                                        <a class="dropdown-item" id="pl" onclick="setLanguage('pl')">Polskie</a>
+                                    </div>
                                 </li>
                             </ul>
                         </div>
